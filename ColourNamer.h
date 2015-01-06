f894abc7535232575e84e69522b4db5c5c505a5e
John Dyer <jbdyer@gmail.com>
2015-01-06 15:00:28 +0000

    step 11

---
+++ ColourNamer.h
@@ -0,0 +1,15 @@
+//
+//  ColourNamer.h
+//  colourNamer
+//
+//  Created by John Dyer on 06/01/2015.
+//  Copyright (c) 2015 John Dyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ViewController.h"
+
+@interface ColourNamer : UIViewController
+@property (weak,nonatomic) ViewController *parent;
+
+@end
---
+++ ColourNamer.m
@@ -0,0 +1,37 @@
+//
+//  ColourNamer.m
+//  colourNamer
+//
+//  Created by John Dyer on 06/01/2015.
+//  Copyright (c) 2015 John Dyer. All rights reserved.
+//
+
+#import "ColourNamer.h"
+
+@interface ColourNamer ()
+
+@end
+
+@implementation ColourNamer
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
